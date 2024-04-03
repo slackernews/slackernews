3ed3f49824c9fffb5d9837f03c5802a67837017f
Chuck D'Antonio <chuck@replicated.com>
2024-04-03 17:32:08 -0400

    Bumps up the debug logging a tiny bit

--- slackernews/lib/metrics/prometheus.ts
+++ slackernews/lib/metrics/prometheus.ts
@@ -29,6 +29,7 @@ const licenseEntitlement = new Gauge({
 
 export async function collectLicenseEntitlements() {
   const entitlements = await ReplicatedClient.listEntitlements();
+  console.log(entitlements)
   for ( var entitlement of entitlements ) {
     var value = entitlement.value ;
     console.log(value)
