ebcc611d1cc89823c8914c85f9e75573121eb9bd
Chuck D'Antonio <chuck@replicated.com>
2024-04-03 16:19:13 -0400

    Sends numeric license entitlements to prom

--- slackernews/lib/metrics/prometheus.ts
+++ slackernews/lib/metrics/prometheus.ts
@@ -1,6 +1,8 @@
 import { Gauge } from 'prom-client';
 import { listDailyActiveUsers, listMonthlyActiveUsers } from "../user";
 
+import { ReplicatedClient, LicenseField } from '../replicated-sdk';
+
 const monthlyUser = new Gauge({
    name: 'slackernews_monthly_user_count',
    help: 'Slackernews monthly active user count',
@@ -17,4 +19,23 @@ export async function collectUserMetrics() {
 
   const monthlyUserList = await listMonthlyActiveUsers();
   monthlyUser.set(monthlyUserList.length);
+}
+
+const licenseEntitlement = new Gauge({
+  name: 'license_entitlement',
+  help: 'Slackernews license entitlement',
+  labelNames: [ 'name', 'title'],
+});
+
+export async function collectLicenseEntitlements() {
+  const entitlements = await ReplicatedClient.listEntitlements();
+  for ( var entitlement of entitlements ) {
+    var value = entitlement.value ;
+    if ( typeof(value) === "number" ) {
+      licenseEntitlement.set({ 
+          "name": entitlement.name, 
+          "title": entitlement.title
+        }, entitlement.value);
+    }
+  }
 }
--- slackernews/pages/api/prometheus.ts
+++ slackernews/pages/api/prometheus.ts
@@ -1,13 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { register, collectDefaultMetrics } from 'prom-client';
 import { sendMetrics } from "../../lib/metrics/replicated";
-import { collectUserMetrics } from "../../lib/metrics/prometheus";
+import { collectUserMetrics, collectLicenseEntitlements } from "../../lib/metrics/prometheus";
 
 // send replicated metrics when sending prometheus metrics
 sendMetrics();
 
 collectDefaultMetrics({ prefix: 'slackernews_' });
 collectUserMetrics();
+collectLicenseEntitlements();
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
     res.setHeader('Content-type', register.contentType);
