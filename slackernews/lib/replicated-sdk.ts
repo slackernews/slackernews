c22ef3f2ee33159614eba81460fabb7a038ce702
Chuck D'Antonio <chuck@replicated.com>
2024-04-04 14:02:52 -0400

    Tears down some debug skaffolding

--- slackernews/lib/replicated-sdk.ts
+++ slackernews/lib/replicated-sdk.ts
@@ -102,16 +102,12 @@ class ReplicatedSdk {
   }
 
   async getUpdates(): Promise<UpdateInfo[]> {
-    console.log("getting updates")
     const response: AxiosResponse<UpdateInfo[]> = await axios.get(`${this.baseURL}/api/v1/app/updates`);
-    console.log("response data: ", response.data)
     return response.data;
   }
 
   async getVersionHistory(): Promise<VersionHistory> {
-    console.log("getting version history")
     const response: AxiosResponse<VersionHistory> = await axios.get(`${this.baseURL}/api/v1/app/history`);
-    console.log("response data: ", response.data)
     return response.data
   }
 
