1afe250c8d027c60afe22d5b78f6284dd36a9f8f
Chuck D'Antonio <chuck@replicated.com>
2024-04-04 12:52:25 -0400

    Shifts metrics back outside of the handler

--- slackernews/pages/api/prometheus.ts
+++ slackernews/pages/api/prometheus.ts
@@ -3,16 +3,16 @@ import { register, collectDefaultMetrics } from 'prom-client';
 import { sendMetrics } from "../../lib/metrics/replicated";
 import { collectUserMetrics, collectLicenseEntitlements } from "../../lib/metrics/prometheus";
 
+// send custom metrics to replicated metrics when sending prometheus metrics
+sendMetrics();
+
+// prepare prometheus metrics
+collectDefaultMetrics({ prefix: 'slackernews_' });
+collectUserMetrics();
+collectLicenseEntitlements();
+collectLicenseEntitlements();
+
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  // send custom metrics to replicated metrics when sending prometheus metrics
-  await sendMetrics();
-
-  // prepare prometheus metrics
-  await collectDefaultMetrics({ prefix: 'slackernews_' });
-  await collectUserMetrics();
-  await collectLicenseEntitlements();
-  await collectLicenseEntitlements();
-
   res.setHeader('Content-type', register.contentType);
   res.setHeader('Cache-Control', 'no-store');
   res.send(await register.metrics());
